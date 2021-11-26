7893c73790cfd5e95f14268f4f9187ce4796387d
Christophe Gandin <cgandin@ceilys.fr>
2021-11-26 18:04:51 +0100

    heroku v3

--- local.js
+++ local.js
@@ -1,25 +1,16 @@
-var path = require('path'), 
-    express = require('express');
+var http = require('http');
+var fs = require('fs');
 
-var app = express();
+// User Heroku port or local port
+const PORT = process.env.PORT || 5000;
 
-// Display on good directory
-app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
-app.use('/wt', express.static(path.join(__dirname, 'walkthrough')));
-app.use('/cv', express.static(path.join(__dirname, 'webapp')));
+fs.readFile('./webapp/index.html', function (err, html) {
 
-// For other key words, go on first page
-app.get('/', function (req, res) {
-	var miniHtml = "Test program of Christophe" + "<BR>"
-		+ "/wt   check if UI5 is active" + "<BR>"
-		+ "/ui5  send to a list (list of Jerry)" + "<BR>"
-		+ "/cv   send to a dynamic CV";
-	res.send(miniHtml);
-});
+    if (err) throw err;    
 
-// Listen to port
-var port = process.env.PORT || 0;
-app.listen(port, function () {
-	console.log("Listen on port " + port);
-	console.log("http://localhost:" + port + "/cv");
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
 });
